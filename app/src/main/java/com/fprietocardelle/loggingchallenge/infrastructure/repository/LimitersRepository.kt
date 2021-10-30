0c83c96ca3c3530f4c6fd85eb3729df563404d61
Francisco <priettt@gmail.com>
2021-10-30 02:51:03 -0300

    Added limiter implementations to repository

--- app/src/main/java/com/fprietocardelle/loggingchallenge/infrastructure/repository/LimitersRepository.kt
+++ app/src/main/java/com/fprietocardelle/loggingchallenge/infrastructure/repository/LimitersRepository.kt
@@ -1,12 +1,23 @@
 package com.fprietocardelle.loggingchallenge.infrastructure.repository
 
-import com.fprietocardelle.loggingchallenge.infrastructure.limiters.LoggingLimiter
+import com.fprietocardelle.loggingchallenge.infrastructure.limiters.CountLimiter
+import com.fprietocardelle.loggingchallenge.infrastructure.limiters.MessageLimiter
+import com.fprietocardelle.loggingchallenge.infrastructure.limiters.RepeatedMessageRateLimiter
+import com.fprietocardelle.loggingchallenge.infrastructure.limiters.TimeLoggingLimiter
+import com.fprietocardelle.loggingchallenge.infrastructure.usecases.GetCurrentTimeInMilliseconds
 import javax.inject.Inject
 
 class LimitersRepository @Inject constructor() {
 
-    private var limiters = mutableListOf<LoggingLimiter>()
+    private val getCurrentTimeInMilliseconds = GetCurrentTimeInMilliseconds()
 
-    fun getLimiters() = limiters.toList()
+    private var limiters = listOf(
+        CountLimiter(20),
+        MessageLimiter(),
+        RepeatedMessageRateLimiter(5000L, getCurrentTimeInMilliseconds),
+        TimeLoggingLimiter(20L, getCurrentTimeInMilliseconds)
+    )
+
+    fun getLimiters() = limiters
 
 }
